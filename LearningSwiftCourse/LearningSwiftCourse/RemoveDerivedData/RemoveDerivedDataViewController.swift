37b0b5c3992fba1402e92829d3d8ee49edf6d5a4
GEORGE QUENTIN <ggeoo93@gmail.com>
2017-08-11 17:56:27 +0100

    added remove derived data

---
+++ LearningSwiftCourse/LearningSwiftCourse/RemoveDerivedData/RemoveDerivedDataViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RemoveDerivedDataViewController.swift
+//  LearningSwiftCourse
+//
+//  Created by GEORGE QUENTIN on 11/08/2017.
+//  Copyright © 2017 LEXI LABS. All rights reserved.
+//
+//https://stackoverflow.com/questions/38016143/how-to-delete-derived-data-in-xcode-8
+
+/*
+
+ If you want to remove derived data manually just run:
+ 
+ rm -rf ~/Library/Developer/Xcode/DerivedData
+ 
+ */
+
+import UIKit
+
+class RemoveDerivedDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
