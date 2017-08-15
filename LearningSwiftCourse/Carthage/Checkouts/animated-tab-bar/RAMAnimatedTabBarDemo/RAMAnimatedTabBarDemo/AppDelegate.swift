e81633366670bde8182cc7930d5e3834a3389f07
GEORGE QUENTIN <ggeoo93@gmail.com>
2017-08-15 01:36:15 +0100

    added new frameworks

---
+++ LearningSwiftCourse/Carthage/Checkouts/animated-tab-bar/RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  RAMAnimatedTabBarDemo
+//
+//  Created by Alex Kalinkin on 11/18/14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    private func application(
+        application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
