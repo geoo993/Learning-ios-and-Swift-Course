26eb0addfe2402951abd9ddc82109b3eac9dd4dc
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-06-19 22:35:32 +0100

    added collage uiimage

--- LearningSwiftCourse/LearningSwiftCourse/Extensions/UIImage+Ext.swift
+++ LearningSwiftCourse/LearningSwiftCourse/Extensions/UIImage+Ext.swift
@@ -10,6 +10,51 @@ import Foundation
 import UIKit
 
 public  extension UIImage {
+
+
+    static func collage(images: [UIImage], size: CGSize) -> UIImage {
+        let rows = images.count < 3 ? 1 : 2
+        let columns = Int(round(Double(images.count) / Double(rows)))
+        let tileSize = CGSize(width: round(size.width / CGFloat(columns)),
+                              height: round(size.height / CGFloat(rows)))
+
+        UIGraphicsBeginImageContextWithOptions(size, true, 0)
+        UIColor.white.setFill()
+        UIRectFill(CGRect(origin: .zero, size: size))
+
+        for (index, image) in images.enumerated() {
+            image.scaled(tileSize).draw(at: CGPoint(
+                x: CGFloat(index % columns) * tileSize.width,
+                y: CGFloat(index / columns) * tileSize.height
+            ))
+        }
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? UIImage()
+    }
+
+    func scaled(_ newSize: CGSize) -> UIImage {
+        guard size != newSize else {
+            return self
+        }
+
+        let ratio = max(newSize.width / size.width, newSize.height / size.height)
+        let width = size.width * ratio
+        let height = size.height * ratio
+
+        let scaledRect = CGRect(
+            x: (newSize.width - width) / 2.0,
+            y: (newSize.height - height) / 2.0,
+            width: width, height: height)
+
+        UIGraphicsBeginImageContextWithOptions(scaledRect.size, false, 0.0);
+        defer { UIGraphicsEndImageContext() }
+
+        draw(in: scaledRect)
+
+        return UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+    }
     
     /// Resizes an image to the specified size.
     ///
